96dcdac570371de6a78e458dd8320d674162e47f
MarcSolde <soldevilla.marc@gmail.com>
2019-11-11 17:50:27 +0100

    wip on instructions

--- go.mod
+++ go.mod
@@ -8,5 +8,5 @@ require (
 	golang.org/x/net v0.0.0-20191108063844-7e6e90b9ea88 // indirect
 	golang.org/x/sys v0.0.0-20191105231009-c1f44814a5cd // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1
 )
--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"encoding/json"
 
 	"github.com/gorilla/mux"
-	"gopkg.in/src-d/go-git.v4"
+	"github.com/libgit2/git2go"
+	// "gopkg.in/src-d/go-git.v4"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
@@ -17,12 +18,24 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 
 func gitWebhook(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body);
-	log.Println(reqBody);
 	var reqBodyUnmarshalled map[string]interface{};
-	json.Unmarshall([]byte(reqBody), &reqBodyUnmarshalled);
+	json.Unmarshal([]byte(reqBody), &reqBodyUnmarshalled);
 	// Print json to play with it
 	// use go git to download the repo to /tmp/<repo_name>
 	fmt.Fprintf(w, "AAAAA: %+v", string(reqBody));
+	fmt.Fprintf(w, "BBBBB: %+v", reqBodyUnmarshalled["ref"]); // Check how to 
+	gitUrl:= "view/comments" // View how to get a nested value a[b][c]??
+	pullUpdates(&gitUrl);
+}
+
+func pullUpdates(gitUrl *string) {
+	// Check if folder exists in file system
+	// if it does not, clone it
+	// if it does, pull the new version
+
+	// Get the info from the yaml conf
+	// Apply using docker sdk
+
 }
 
 func main() {
